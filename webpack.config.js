983e7ddcd3f64903fa01dfd645c929c7c49f5f1f
Jo-Myounghee <jomyounghee32@gmail.com>
2021-09-07 14:42:13 +0900

    Feat: 웹팩 실행 모드 설정
    개발 모드(build:dev), 배포 모드(build)로 나누어 웹팩 실행 모드 설정
    https://joshua1988.github.io/webpack-guide/advanced/mode-config.html#%EC%8B%A4%ED%96%89-%EB%AA%A8%EB%93%9C%EC%97%90-%EB%94%B0%EB%9D%BC-%EC%9B%B9%ED%8C%A9-%EC%84%A4%EC%A0%95-%EB%8B%AC%EB%A6%AC%ED%95%98%EA%B8%B0
    
    #5

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require("path");
 
-module.exports = {
-  entry: {
-    main: ["./src/index.js"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = (env) => {
+  let entryPath = env.mode === 'production'
+    ? './public/index.js'
+    : './src/index.js';
+
+  return {
+    entry: entryPath,
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "build"),
+      clean: true,
+    },
+    module: {
+      rules: [
+        { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
-        }
-      },
-    ]
-  },
-  devtool: 'cheap-eval-source-map'
+        },
+      ]
+    },
+    devtool: 'cheap-eval-source-map',
+  }
 };
