859ef6624c6545d1860705f5f99d404dc5da49b8
Jo-Myounghee <jomyounghee32@gmail.com>
2021-09-08 19:32:55 +0900

    Fix: build 실행 모드 제거
    build 실행 모드를 설정(#5)하였으나, 해당 실행 모드가 적용되지 않는 것을 확인 후 실행모드를 제거(default는 production모드)
    
    #9

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = env => {
-  const entryPath =
-    env.mode === 'production' ? './public/index.js' : './src/index.js';
-
-  return {
-    entry: ['@babel/polyfill', entryPath],
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'build'),
-      clean: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js', './src/style.scss'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
         }
       ]
